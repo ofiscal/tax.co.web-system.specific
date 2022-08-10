b03244e924536ec2b5984375f63c0fb9e54917b1
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2022-08-10 16:16:15 -0500

    Add a comment.
      PITFALL: This change was made to master,
      which meant putting the makefile_targets.py file on that branch.
      Don't let that fool you into thinking the master branch should be used
      for anything except pushing changes to the other branches, though.

---
+++ makefile_targets.py
@@ -0,0 +1,7 @@
+# For the full list of possible targets,
+# see the Makefile, particularly the definition of .PHONY.
+targets = [ "show_config",
+            "show_params",
+            "tests",
+            "reports",
+           ]
